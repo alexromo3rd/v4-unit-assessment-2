41d99a08462bb0434ce195697160dc6a6b9807c0
Alex Romo III <alexromo3rd@gmail.com>
2020-11-19 19:03:04 -0800

    Step 1 complete

--- js-in-the-dom/index.js
+++ js-in-the-dom/index.js
@@ -1,1 +1,13 @@
+let formBtn = document.getElementById('close-form');
+let form = document.querySelector('form');
+
+let toggler = () => {
+  form.classList.toggle('hide');
+  formBtn.innerText === 'X' ? formBtn.innerText = '+' : formBtn.innerText = 'X';
+}
+
+let nameInput = document.querySelector('#name');
+let emailInput = document.querySelector('#email');
+
+// console.log(nameInput, emailInput);
 
