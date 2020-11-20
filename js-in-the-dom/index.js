024fd98a75cf7ab521c48bc91c1b2c81ae228965
Alex Romo III <alexromo3rd@gmail.com>
2020-11-19 19:25:27 -0800

    Step 2 complete

--- js-in-the-dom/index.js
+++ js-in-the-dom/index.js
@@ -8,6 +8,23 @@ let toggler = () => {
 
 let nameInput = document.querySelector('#name');
 let emailInput = document.querySelector('#email');
+let emailList = [];
 
-// console.log(nameInput, emailInput);
+let validateForm = (e) => {
+  e.preventDefault();
+  if (!nameInput.value && !emailInput.value) {
+    nameInput.style.border = '2px solid red';
+    emailInput.style.border = '2px solid red';
+    alert('You must enter a name and an email address to subscribe!');
+  } else if (!nameInput.value) {
+    nameInput.style.border = '2px solid red';
+    alert('Please enter a name to subscribe!');
+  } else if (!emailInput.value) {
+    emailInput.style.border = '2px solid red';
+    alert('Please enter an email address to subscribe!');
+  } else {
+    emailList.push({name: nameInput.value, email: emailInput.value})
+  }
+}
 
+form.addEventListener('submit', validateForm);
